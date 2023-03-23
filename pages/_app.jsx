c4389f8a9848578fde8e9b64012f4aa0acc9bbb2
damagesavage <mai254na@gmail.com>
2023-03-23 05:39:18 +0300

    added new git ignore

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,13 @@
 import { SessionProvider } from "next-auth/react";
 import "./styles.css";
-import { StoreProvider } from "./../store/storeContext";
-import type { AppProps } from "next/app";
-import type { Session } from "next-auth";
+import { StoreProvider } from "../store/storeContext";
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps<{ session: Session }>) {
+}) {
   const getLayout = Component.getLayout || ((page) => page);
   return (
     <SessionProvider session={session}>
